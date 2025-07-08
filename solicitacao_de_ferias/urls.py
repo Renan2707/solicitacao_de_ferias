2e5ae5b4b03843cd5cc62919d7cb2ff180b00dd2
renan <renan.silveira@macrosul.com>
2025-07-08 16:32:32 -0300

    organizando as views

--- solicitacao_de_ferias/urls.py
+++ solicitacao_de_ferias/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views.index import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
