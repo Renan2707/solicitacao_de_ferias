3eccfc978dff78502047a3420eab07ffb5249f49
renan <renan.silveira@macrosul.com>
2025-07-10 11:58:43 -0300

    ajuste no models de card

--- core/models/card.py
+++ core/models/card.py
@@ -3,16 +3,19 @@ from django.contrib.auth.models import User
 
 class Card(models.Model):
     setor = models.CharField()
-    data_de_admissao = models.CharField(blank=False, null=False)
-    data_de_vencimento_de_ferias= models.CharField(blank=False, null=False)
+    nome = models.CharField(blank=True, null=True)
+    email = models.CharField(blank=True, null=True)
+    data_de_admissao = models.DateTimeField(blank=False, null=False)
+    data_de_vencimento_de_ferias= models.DateTimeField(blank=False, null=False)
     saldo_de_ferias = models.CharField(blank=False, null=False)
     colaborador = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='cards')
+    data_de_vencimento_proxima = models.BooleanField(default=False)
     
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.colaborador
+        return self.nome
     class Meta:
         verbose_name = "Card"
         verbose_name_plural = "Cards"
