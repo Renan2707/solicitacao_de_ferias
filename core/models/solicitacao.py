4fc0fefc374097141091272eb6841ecc484b1947
renan <renan.silveira@macrosul.com>
2025-07-10 15:25:05 -0300

    urls e novo models de solicitação

--- core/models/solicitacao.py
+++ core/models/solicitacao.py
@@ -3,17 +3,27 @@ from django.contrib.auth.models import User
 from core.models.card import Card
 
 class SolicitacaoDeFerias(models.Model):
+    #CAMPOS PREENCHIDOS PELO COLABORADOR:
     dias_de_descanso = models.CharField(blank=False, null=False)
     dias_vendidos = models.CharField(blank=False, null=False)
     email_gestor= models.CharField(blank=False, null=False)
-    email_colaborador = models.CharField(blank=False, null=False)
-    nome_colaborador = models.CharField( blank=False, null=False)
+    inicio_do_descanso = models.DateField(blank=False, null=True)
+    observacao_do_gestor= models.CharField(blank=True, null=True)
+    comunicacao_com_gestor = models.FileField(blank=True, null=True)
+
+    # CAMPOS PREENCHIDOS AUTOMATICAMENTE OU NAS VERIFICAÇÕES:
+    user= models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE,)
+    fim_do_descanso = models.DateField(blank=True, null=True)
+    solicitacao_aprovada = models.BooleanField(default=False)
+    ferias_iniciadas = models.BooleanField(default=False)
+    ferias_finalizadas = models.BooleanField(default=False)
+    ferias_rejeitadas = models.BooleanField(default=False)
     card = models.ForeignKey(Card, blank=True, null=True, on_delete=models.CASCADE, related_name='solicitacoes_de_ferias')
     
     criado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.nome_colaborador
+        return self.user.username
     
     class Meta:
         verbose_name = "Solicitacao De Ferias"
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from core.views.cards import add_colaborador, vencimento_proximo
 
+from core.views.solicitacao import add_solicitacao
+
 urlpatterns = [
    path('add_colaborador/', add_colaborador, name='add_colaborador'),
    path('vencimento_proximo/', vencimento_proximo, name='vencimento_proximo'),
+   path('add_solicitacao/', add_solicitacao, name='add_solicitacao'),
 ]
