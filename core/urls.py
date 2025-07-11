20d4e1c09653b27b3f1c99206745d4982015ba06
renan <renan.silveira@macrosul.com>
2025-07-11 11:30:44 -0300

    urls de verificão e aprovação das solicitações

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from core.views.cards import add_colaborador, vencimento_proximo
 
-from core.views.solicitacao import add_solicitacao
+from core.views.solicitacao import add_solicitacao, reprovar_solicitacao, aprovar_solicitacao, verificar_fim_das_ferias
 
 urlpatterns = [
    path('add_colaborador/', add_colaborador, name='add_colaborador'),
    path('vencimento_proximo/', vencimento_proximo, name='vencimento_proximo'),
    path('add_solicitacao/', add_solicitacao, name='add_solicitacao'),
+   path('reprovar_solicitacao/<str:id_solicitacao>/', reprovar_solicitacao, name='reprovar_solicitacao'),
+   path('aprovar_solicitacao/<str:id_solicitacao>/', aprovar_solicitacao, name='aprovar_solicitacao'),
+   path('verificar_fim_das_ferias/', verificar_fim_das_ferias, name='verificar_fim_das_ferias'),
 ]
