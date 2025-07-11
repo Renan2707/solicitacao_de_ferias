50bffd8911a656ee015cf10140ac50ce53be30bd
renan <renan.silveira@macrosul.com>
2025-07-11 11:32:04 -0300

    view de aprovações e verificação das solicitações

--- core/views/solicitacao.py
+++ core/views/solicitacao.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from core.forms import SolicitacaoDeFeriasForm
+from core.forms import SolicitacaoDeFeriasForm, VerificaSolicitacaoForm
+from core.models.solicitacao import SolicitacaoDeFerias
 from core.models.card import Card
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 @login_required
 def add_solicitacao(request):
@@ -22,3 +23,55 @@ def add_solicitacao(request):
     else:
         form = SolicitacaoDeFeriasForm()
         return render(request, 'core/index.html', {'form': form})
+    
+
+@login_required
+def reprovar_solicitacao(request, id_solicitacao):
+    solicitacao = SolicitacaoDeFerias.objects.get(pk=id_solicitacao)
+    solicitacao.ferias_rejeitadas = True
+    solicitacao.save()
+    return redirect(reverse('index'))
+
+@login_required
+def aprovar_solicitacao(request, id_solicitacao):
+    solicitacao = SolicitacaoDeFerias.objects.get(pk=id_solicitacao)
+
+    if request.method == 'POST':
+        form = VerificaSolicitacaoForm(request.POST)
+        if form.is_valid():
+            #APROVANDO A SOLICITAÇÃO
+            solicitacao.solicitacao_aprovada = True
+            #ATUALIZANDO O SALDO DO CARD
+            saldo = int( solicitacao.card.saldo_de_ferias)
+            dias_descanso_solicitados = int(solicitacao.dias_de_descanso)
+            dias_venda_solicitados = int(solicitacao.dias_vendidos)
+            dias_totais_solicitados = dias_descanso_solicitados + dias_venda_solicitados
+            solicitacao.card.saldo_de_ferias = saldo - dias_totais_solicitados
+            #SALVANDO CARD E SOLICITACAO
+            solicitacao.save()
+            solicitacao.card.save()
+            #VERIFICANDO SE AS FÉRIAS JÁ PODEM SER INICIADAS
+            verificar_inicio_das_ferias(request)
+            return redirect(reverse('index'))
+        else:
+            return render(request, 'core/index.html', {'form': form, 'form_errors': form.errors})
+    return redirect(reverse('index'))
+
+
+def verificar_inicio_das_ferias(request):
+    solicitacoes = SolicitacaoDeFerias.objects.filter(solicitacao_aprovada=True)
+    for solicitacao in solicitacoes:
+        if solicitacao.inicio_do_descanso <= datetime.now().date() and not solicitacao.ferias_iniciadas:
+            solicitacao.ferias_iniciadas = True
+            solicitacao.save()
+    return redirect(reverse('index'))
+
+
+
+def verificar_fim_das_ferias(request):
+    solicitacoes = SolicitacaoDeFerias.objects.filter(ferias_iniciadas=True)
+    for solicitacao in solicitacoes:
+        if solicitacao.fim_do_descanso <= datetime.now().date():
+            solicitacao.ferias_finalizadas = True
+            solicitacao.save()
+    return redirect(reverse('index'))    
