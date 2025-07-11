e9a785726869197932403d759ce93902d5453b9b
renan <renan.silveira@macrosul.com>
2025-07-11 11:29:59 -0300

    criiando form de verificação da solicitação de férias

--- core/forms.py
+++ core/forms.py
@@ -12,3 +12,10 @@ class SolicitacaoDeFeriasForm(forms.ModelForm):
     class Meta:
         model = SolicitacaoDeFerias
         fields = ['user','dias_de_descanso', 'dias_vendidos', 'email_gestor', 'inicio_do_descanso', 'observacao_do_gestor', 'comunicacao_com_gestor', 'card','fim_do_descanso', 'solicitacao_aprovada', 'ferias_iniciadas', 'ferias_finalizadas', 'ferias_rejeitadas']
+
+class VerificaSolicitacaoForm(forms.Form):
+    saldo_de_ferias_necessario =  forms.BooleanField(required=True)
+    quantidade_de_dias_para_venda =  forms.BooleanField(required=True)
+    data_de_solicitacao_valida =  forms.BooleanField(required=True)
+    comunicou_o_gestor = forms.BooleanField(required=True)
+    aprovado_pela_diretoria = forms.BooleanField(required=True)
