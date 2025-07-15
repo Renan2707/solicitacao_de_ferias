12e47557dc065d470d87fc5bf229053999830f1b
renan <renan.silveira@macrosul.com>
2025-07-15 16:56:51 -0300

    criando url para verificar inicio das férias

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from core.views.cards import add_colaborador, vencimento_proximo, renova_saldo_de_ferias, edit_colaborador, remove_colaborador
 
-from core.views.solicitacao import add_solicitacao, reprovar_solicitacao, aprovar_solicitacao, verificar_fim_das_ferias
+from core.views.solicitacao import add_solicitacao, reprovar_solicitacao, aprovar_solicitacao, verificar_fim_das_ferias, verificar_inicio_das_ferias
 
 urlpatterns = [
    path('add_colaborador/', add_colaborador, name='add_colaborador'),
@@ -13,5 +13,6 @@ urlpatterns = [
    path('reprovar_solicitacao/<str:id_solicitacao>/', reprovar_solicitacao, name='reprovar_solicitacao'),
    path('aprovar_solicitacao/<str:id_solicitacao>/', aprovar_solicitacao, name='aprovar_solicitacao'),
    path('verificar_fim_das_ferias/', verificar_fim_das_ferias, name='verificar_fim_das_ferias'),
+   path('verificar_inicio_das_ferias/', verificar_inicio_das_ferias, name='verificar_inicio_das_ferias'),
    path('renova_saldo_de_ferias/', renova_saldo_de_ferias, name='renova_saldo_de_ferias'),
 ]
