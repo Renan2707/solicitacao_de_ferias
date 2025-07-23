d8cd75dbdfef0efaa5f98e32bbe01028438916fa
renan <renan.silveira@macrosul.com>
2025-07-23 16:53:23 -0300

    adicionando js de desabilitar o botão para 'data.js'

--- static/js/data.js
+++ static/js/data.js
@@ -5,3 +5,18 @@ function data(){
         dropDownHistorico: false,
     }
 }
+
+
+// DESABILITANDO OS BOTÕES DOS FORMULÁRIOS 
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".preventForm").forEach(function (form) {
+        form.addEventListener("submit", function (e) {
+        const submitBtn = form.querySelector('button[type="submit"]');
+        if (submitBtn) {
+            submitBtn.disabled = true;
+            submitBtn.classList.add('opacity-50', 'cursor-not-allowed');
+            submitBtn.innerHTML = '<i class="fa-solid fa-rotate-right fa-spin"></i>';
+        }
+        });
+    });
+});
