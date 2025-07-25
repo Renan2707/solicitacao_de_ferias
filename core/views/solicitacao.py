717ba8f19f1f0e4d7b5615db6a7f9d8571a83af5
renan <renan.silveira@macrosul.com>
2025-07-25 16:11:38 -0300

    verificando se o dia solicitado é 48h antes de algum feriado

--- core/views/solicitacao.py
+++ core/views/solicitacao.py
@@ -5,7 +5,46 @@ from core.forms import SolicitacaoDeFeriasForm, VerificaSolicitacaoForm
 from core.models.solicitacao import SolicitacaoDeFerias
 from core.models.card import Card
 from datetime import timedelta, datetime
+import requests
 from core.views.emails import email_nova_solicitacao, email_solicitacao_reprovada, email_solicitacao_aprovada
+
+def verifica_feriados(data_inicio_das_ferias):
+    ano_atual = datetime.now().year
+    proximo_ano = datetime.now().year + 1
+    url_ano_atual = f'https://brasilapi.com.br/api/feriados/v1/{ano_atual}'
+    url_proximo_ano = f'https://brasilapi.com.br/api/feriados/v1/{proximo_ano}'
+
+    feriados_curitiba = [
+        {
+            "date":f"{ano_atual}-09-08",
+            "nome":"Nossa Senhora da Luz",
+        },
+        {
+            "date":f"{proximo_ano}-09-08",
+            "nome":"Nossa Senhora da Luz",
+        },
+    ]
+
+    try:
+        response_atual = requests.get(url_ano_atual)
+        feriados_atuais = response_atual.json()
+
+        response_proximo_ano = requests.get(url_proximo_ano)
+        feriados_proximo_ano = response_proximo_ano.json()
+    except:
+        erro = 'A API para verificação de datas está fora do ar. Tente novamente mais tarde, ou entre em contato com a Equipe de Desenvolvedores Macrosul.'
+        return erro
+
+    todos_os_feriados = feriados_atuais + feriados_proximo_ano + feriados_curitiba
+    datas_feriados = [datetime.strptime(f["date"], "%Y-%m-%d").date() for f in todos_os_feriados]
+
+    for feriado in datas_feriados:
+            diferenca = (feriado - data_inicio_das_ferias).days
+            if 0 <= diferenca <= 2:  # feriado no mesmo dia, ou 1 ou 2 dias depois
+                return False  # Não pode iniciar férias nesse período
+
+    return True  # OK: não é feriado nem até 48h antes
+
 
 @login_required
 def add_solicitacao(request):
@@ -28,10 +67,24 @@ def add_solicitacao(request):
             # VERIFICA SE TEM SALDO DE FÉRIAS SUFICIENTE
             if int(card_usuario.saldo_de_ferias) < int(solicitacao.dias_de_descanso) + int(solicitacao.dias_vendidos):
                 return render(request, 'core/index.html', {'saldo_de_ferias_insuficiente': True, 'form':form })
-            else:
+            
+            #VERIFICA SE O DIA DE INICIO É FERIADO
+            verificacao_de_feriado = verifica_feriados(solicitacao.inicio_do_descanso)
+
+            if verificacao_de_feriado is True:
                 solicitacao.save()
                 email_nova_solicitacao(solicitacao)
                 return render(request, 'core/index.html', {'form': form,'success': True })
+            elif verificacao_de_feriado is False:
+                return render(request, 'core/index.html', {'form': form, 'feriado': True})
+            elif isinstance(verificacao_de_feriado, str):
+                return render(request, 'core/index.html', {'form': form, 'erro_api': verificacao_de_feriado})
+            else:
+                # Fallback de segurança
+                return render(request, 'core/index.html', {
+                    'form': form,
+                    'erro_api': f'Erro inesperado ao verificar feriados.'
+                })
         else:
             return render(request, 'core/index.html', {'form': form, 'form_errors': form.errors})
     else:
